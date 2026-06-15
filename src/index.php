b7b225f7ff2d725d4de33e2c10951f55589deec3
Szymon <s101587@pollub.edu.pl>
2026-06-15 18:53:20 +0200

    Add: added functional index.php

--- src/index.php
+++ src/index.php
@@ -1,2 +1,15 @@
 <?php
+$host = 'mysql';
+$user = trim(file_get_contents('/run/secrets/user'));
+$pass = trim(file_get_contents('/run/secrets/userpass'));
+$db = trim(file_get_contents('/run/secrets/db'));
+
 echo "<p>101587</p>";
+
+try{
+  $mysqli = new mysqli($host,$user,$pass,$db);
+  echo $mysqli->server_info . "\nNazwa bazy: " . $db
+  $mysqli->close();
+}catch(Exception $e){
+  echo $e->getMessage();
+}
